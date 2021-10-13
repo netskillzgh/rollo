f79f68907137e8c987b6a435ab1fd073207687ed
Netskillzgh <netskillz@protonmail.com>
2021-10-13 17:22:22 +0200

    remove rayon

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -22,6 +22,7 @@ async fn main() {
     }));
 
     let mut socket_manager = WorldSocketMgr::new(world);
+    // Run the server and the game loop with an interval (15ms)
     socket_manager
         .start_game_loop(Duration::from_millis(15))
         .start_network("127.0.0.1:6666", ListenerSecurity::Tcp)
