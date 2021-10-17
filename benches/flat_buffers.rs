bbdccb54a8f38c992b5ce2924c0706c4e143a899
Netskillzgh <netskillz@protonmail.com>
2021-10-17 17:42:25 +0200

    clippy

--- benches/flat_buffers.rs
+++ benches/flat_buffers.rs
@@ -3,7 +3,7 @@ use rollo::flat_buffers_helpers::{generate_builders, get_builder};
 
 fn basic_get(c: &mut Criterion) {
     generate_builders(1);
-    c.bench_function("flat_buffers", |b| b.iter(|| get_builder()));
+    c.bench_function("flat_buffers", |b| b.iter(get_builder));
 }
 
 criterion_group!(benches, basic_get);
