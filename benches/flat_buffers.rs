26b7c7f813d05a4053311227d0c0928a159f90ec
Netskillzgh <netskillz@protonmail.com>
2021-10-21 15:52:24 +0200

    Improve bench

--- benches/flat_buffers.rs
+++ benches/flat_buffers.rs
@@ -1,12 +1,13 @@
 use criterion::{criterion_group, criterion_main, Criterion};
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
 use rollo::flatbuffers_pool;
 
 fn basic_get(c: &mut Criterion) {
-    flatbuffers_pool!(100, TEST, get_builder);
+    flatbuffers_pool!(1000, TEST, get_builder);
     generate_builders(1);
     c.bench_function("flat_buffers", |b| {
         b.iter(|| {
-            (0..50).into_iter().for_each(|_| {
+            (0..500).into_par_iter().for_each(|_| {
                 get_builder();
             })
         })
--- benches/rand.rs
+++ benches/rand.rs
@@ -1,10 +1,11 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
 use rollo::game::roll;
 
 fn basic_rand(c: &mut Criterion) {
     c.bench_function("basic_rand", |b| {
         b.iter(|| {
-            (0..50).into_iter().for_each(|_| {
+            (0..200).into_par_iter().for_each(|_| {
                 roll(black_box(20.0));
             })
         })
