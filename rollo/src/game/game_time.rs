1238fe8f3741075f73635ca3f5774167c453c1d9
Netskillzgh <netskillz@protonmail.com>
2021-10-24 17:14:13 +0200

    Add tests

--- rollo/src/game/game_time.rs
+++ rollo/src/game/game_time.rs
@@ -1,5 +1,6 @@
 use std::time::{Duration, Instant, SystemTime, SystemTimeError, UNIX_EPOCH};
 
+/// GameTime
 #[derive(Debug, Clone, Copy)]
 pub struct GameTime {
     instant: Instant,
@@ -9,6 +10,9 @@ pub struct GameTime {
 }
 
 impl GameTime {
+    /// ```rust, no_run
+    /// let game_time = GameTime::new();
+    /// ```
     pub fn new() -> Self {
         let duration = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
         Self {
@@ -19,11 +23,14 @@ impl GameTime {
         }
     }
 
-    pub(crate) fn update_time(&mut self) {
+    pub(crate) fn update_time(&mut self) -> bool {
         if let Ok(duration) = Self::current_timestamp() {
             self.system_time = duration;
             self.timestamp = duration.as_millis() as i64;
             self.elapsed = self.instant.elapsed();
+            true
+        } else {
+            false
         }
     }
 
@@ -35,5 +42,44 @@ impl GameTime {
 impl Default for GameTime {
     fn default() -> Self {
         Self::new()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::thread::sleep;
+
+    use super::*;
+
+    #[test]
+    fn test_new() {
+        let game_time = GameTime::new();
+        assert_eq!(
+            game_time.timestamp,
+            game_time.system_time.as_millis() as i64
+        );
+        assert_eq!(game_time.elapsed, Duration::ZERO);
+    }
+
+    #[test]
+    fn test_current_timestamp() {
+        let timestamp = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+        assert!(GameTime::current_timestamp().unwrap() >= timestamp);
+    }
+
+    #[test]
+    fn test_update_time() {
+        let mut game_time = GameTime::new();
+        let timestamp = game_time.timestamp;
+        let ellapsed = game_time.elapsed;
+        let system_time = game_time.system_time;
+        assert_eq!(ellapsed, Duration::ZERO);
+        sleep(Duration::from_millis(1));
+
+        let r = game_time.update_time();
+        assert!(r);
+        assert!(game_time.timestamp > timestamp);
+        assert!(game_time.elapsed > ellapsed);
+        assert!(game_time.system_time > system_time);
     }
 }
