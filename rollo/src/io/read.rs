4ba09e221791a6cb96fd53e91d74a0b66351afd0
Rollon <netskillz@protonmail.com>
2022-04-18 18:32:31 +0200

    The if wasn't necessary.

--- rollo/src/io/read.rs
+++ rollo/src/io/read.rs
@@ -65,6 +65,7 @@ where
     where
         R: AsyncReadExt + Unpin,
     {
+        debug_assert!(size != 0);
         debug_assert!(self.payload.len() == MAX_SIZE);
 
         let size = self
@@ -72,10 +73,6 @@ where
             .read_exact(&mut self.payload[0..size])
             .await
             .map_err(|_| Error::PacketSize)?;
-
-        if size == 0 {
-            return Err(Error::PacketSize);
-        }
 
         let mut payload = POOL_VEC_PACKET.create();
         debug_assert!(payload.is_empty());
